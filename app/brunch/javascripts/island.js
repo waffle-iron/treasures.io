da756f8ef7a07ea3d3c3b81d8a294b3e2b3b8f0f
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2016-11-03 17:34:08 +0100

    an island

---
+++ app/brunch/javascripts/island.js
@@ -0,0 +1,22 @@
+export class Island extends PIXI.Graphics {
+  constructor() {
+    super()
+    this.beginFill(0xf1c40f)
+
+    //Use `drawPolygon` to define the triangle as
+    //a path array of x/y positions
+
+    let points = []
+    for (var i = 0; i < 100; i++) {
+      points.push(Math.random()*300)
+    }
+    this.drawPolygon(points)
+
+    this.x = 50
+    this.y = 60
+    this.width = 300
+    this.height = 300
+
+    this
+  }
+}
--- app/brunch/javascripts/world_map.js
+++ app/brunch/javascripts/world_map.js
@@ -1,3 +1,5 @@
+import {Island} from './island.js'
+
 export class WorldMap {
   constructor(element, verticalSiblings) {
     this.element = element
@@ -5,12 +7,17 @@ export class WorldMap {
 
     // Create the renderer
     this.renderer = PIXI.autoDetectRenderer(this.element.offsetWidth, this.mapfillHeight())
-
+    this.renderer.backgroundColor = 0x2980b9
     // Add the canvas to the HTML document
     this.element.appendChild(this.renderer.view)
 
     this.stage = new PIXI.Container()
+
+    this.stage.addChild(new Island())
+
     this.renderer.render(this.stage)
+    this.renderer.autoResize = true
+
     let resize = () => {
       this.renderer.resize(this.element.offsetWidth, this.mapfillHeight())
     }
