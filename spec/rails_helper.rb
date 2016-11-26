aa1e6a9372c963746a30b4f0d71f16cadf018e74
Andreas Knöpfle <andreas.knoepfle@googlemail.com>
2016-11-26 15:28:09 +0100

    repair coverage (#12)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -6,9 +9,6 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
-require 'simplecov'
-SimpleCov.start
 
 if ENV.fetch('CI', false)
   require 'codecov'
